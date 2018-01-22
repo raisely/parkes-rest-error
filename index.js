c98e684d135bc9b3d9c6c0de99c5c49223e0161e
Flynn Buckingham <dev@flynnbuckingham.com>
2018-01-22 16:01:07 -0500

    Added custom error mapping and formatter support

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const ErrorHandler = require('./lib/errorHandler');
 const RestError = require('./lib/restError');
+const { addErrorCode } = require('./lib/errorCodes');
+const { addFormat } = require('./lib/formatError');
 
-module.exports = { ErrorHandler, RestError };
+module.exports = { ErrorHandler, RestError, addErrorCode, addFormat };
